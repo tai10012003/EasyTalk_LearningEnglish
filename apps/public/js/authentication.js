e18f5d4760e58ca7791c5e9dd0e2a967a81679b2
tai10012003 <pductai14@gmail.com>
2025-10-16 12:03:32 +0700

    delete css, js, interface in backend

--- apps/public/js/authentication.js
+++
@@ -1,8 +0,0 @@
-// Kiểm tra token ngay khi trang được tải
-(function() {
-    const token = localStorage.getItem("token");
-    if (!token) {
-        alert("Bạn cần đăng nhập để truy cập trang này.");
-        window.location.href = "/login";
-    }
-})();
