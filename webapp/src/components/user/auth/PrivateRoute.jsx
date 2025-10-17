f1a065ceaed1b94ede338cd321ae06ba6d970271
tai10012003 <pductai14@gmail.com>
2025-10-17 21:09:46 +0700

    fix private route

--- webapp/src/components/user/auth/PrivateRoute.jsx
+++ webapp/src/components/user/auth/PrivateRoute.jsx
@@ -1,31 +1,29 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { Navigate } from "react-router-dom";
 import Swal from "sweetalert2";
 
 const PrivateRoute = ({ children, roles = [] }) => {
     const token = localStorage.getItem("token");
+    const [hasAlertShown, setHasAlertShown] = useState(false);
     const role = localStorage.getItem("role");
-    const [redirectTo, setRedirectTo] = useState(null);
-    const [alertShown, setAlertShown] = useState(false);
 
-    useEffect(() => {
-        if (!token && !alertShown) {
-            setAlertShown(true);
+    if (!token || !roles) {
+        if (!hasAlertShown) {
             Swal.fire({
                 icon: "warning",
                 title: "Cảnh báo",
                 text: "Bạn cần đăng nhập để truy cập trang này!",
                 confirmButtonText: "Đồng ý",
-            }).then(() => {
-                setRedirectTo("/login");
             });
+            setHasAlertShown(true);
         }
-        else if (token && roles.length > 0 && !roles.includes(role)) {
-            setRedirectTo("/");
+        return <Navigate to="/login" replace />;
+    }
+    if (roles.length > 0 && !roles.includes(role)) {
+        if (!hasAlertShown) {
+            setHasAlertShown(true);
         }
-    }, [token, role, roles, alertShown]);
-    if (!token || (roles.length > 0 && !roles.includes(role))) {
-        return redirectTo ? <Navigate to={redirectTo} replace /> : null;
+        return <Navigate to="/" replace />;
     }
     return children;
 };
