7688b76121834136a99ecb6f14ca00a0847d91a3
tai10012003 <pductai14@gmail.com>
2025-10-17 08:46:04 +0700

    update route

--- apps/app.js
+++ apps/app.js
@@ -18,9 +18,57 @@ app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 var controller = require(__dirname  + "/controllers");
 app.use(controller);
 
+var journeyController = require(__dirname + "/controllers/journeycontroller");
+app.use(journeyController);
+
+var gateController = require(__dirname + "/controllers/gatecontroller");
+app.use(gateController);
+
+var stageController = require(__dirname + "/controllers/stagecontroller"); 
+app.use(stageController);
+
+var storyController = require(__dirname + "/controllers/storycontroller");
+app.use(storyController);
+
+var grammarController = require(__dirname + "/controllers/grammarcontroller");
+app.use(grammarController);
+
+var grammarexerciseController = require(__dirname + "/controllers/grammarexercisecontroller");
+app.use(grammarexerciseController);
+
+var pronunciationController = require(__dirname + "/controllers/pronunciationcontroller");
+app.use(pronunciationController);
+
+var pronunciationexerciseController = require(__dirname + "/controllers/pronunciationexercisecontroller");
+app.use(pronunciationexerciseController);
+
+var flashcardController = require(__dirname + "/controllers/flashcardcontroller");
+app.use(flashcardController);
+
+var vocabularyexerciseController = require(__dirname + "/controllers/vocabularyexercisecontroller");
+app.use(vocabularyexerciseController);
+
+var dictationexerciseController = require(__dirname + "/controllers/dictationcontroller");
+app.use(dictationexerciseController);
+
+var userController = require(__dirname + "/controllers/usercontroller");
+app.use(userController);
+
+var chatController = require(__dirname + "/controllers/chatcontroller");
+app.use(chatController);
+
+var writingController = require(__dirname + "/controllers/writingcontroller");
+app.use(writingController);
+
+var reminderController = require(__dirname + "/controllers/remindercontroller");
+app.use(reminderController);
+
+var userprogressController = require(__dirname + "/controllers/userprogresscontroller");
+app.use(userprogressController);
+
 app.use("/static", express.static(__dirname + "/public"));
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
