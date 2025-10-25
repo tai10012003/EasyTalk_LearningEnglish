d64f6234012a79acca5b4506c40d565251fe2e66
tai10012003 <pductai14@gmail.com>
2025-10-25 11:38:47 +0700

    add language switching feature

--- webapp/src/layouts/UserLayout.jsx
+++ webapp/src/layouts/UserLayout.jsx
@@ -1,16 +1,19 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
 import Menu from "@/components/user/Menu";
 import Footer from "@/components/user/Footer";
 import { Outlet } from "react-router-dom";
 
 function UserLayout() {
+  const { t } = useTranslation();
+
   return (
     <div>
-        <Menu />
-        <main>
-            <Outlet />
-        </main>
-        <Footer />
+      <Menu />
+      <main aria-label={t("mainContent")}>
+        <Outlet />
+      </main>
+      <Footer />
     </div>
   );
 }
--- webapp/src/main.jsx
+++ webapp/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import '@/index.css'
 import '@/admin.css'
 import App from '@/App.jsx'
+import "./i18n";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
